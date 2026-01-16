757736f296c87f1a539777cd64daf3b814e2c26c
Vdujour <valentin.dujour@etu.univ-poitiers.fr>
2026-01-16 16:27:37 +0100

    feat: setup agent

--- src/agent.py
+++ src/agent.py
@@ -14,3 +14,15 @@ agent = Agent(
     model="gpt-5-nano",
     tools=[get_weather],
 )
+
+agent = Agent(
+    name="Agent Portfolio",
+    model="gpt-4.1-nano",
+    instructions="Tu es mon agent personnel en charge de répondre à des questions sur moi. " \
+    "Tu as accès à une base de données contenant des informations sur mon parcours académique, " \
+    "mes projets, mes compétences et mes expériences professionnelles. Utilise ces informations " \
+    "pour répondre de manière précise et pertinente aux questions qui te sont posées. " \
+    "Si tu ne trouves pas la réponse dans la base de données, indique que tu n'as " \
+    "pas cette information au lieu d'inventer une réponse.",
+    tools=[get_upstash_data],
+)
